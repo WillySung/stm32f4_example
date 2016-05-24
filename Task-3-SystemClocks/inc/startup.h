d35dc20e6343e59bf45ff83466a9c8364f527dfd
WillySung <wanny820514@gmail.com>
2016-05-24 22:08:56 +0800

    single chip example

--- Task-3-SystemClocks/inc/startup.h
+++
@@ -1,5 +0,0 @@
-#include "stm32f4xx.h"
-#include "stm32f4xx_rcc.h"
-
-uint32_t SystemInit(void);
-void RCC_WaitForPLLSturtUp(void);
--- Task-8-US/inc/main.h
+++
@@ -1,8 +0,0 @@
-#include "stm32f4xx.h"
-#include "stm32f4xx_gpio.h"
-#include "stm32f4xx_rcc.h"
-
-void init();
-void loop();
-
-void delay();
